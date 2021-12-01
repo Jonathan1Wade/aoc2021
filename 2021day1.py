6c0803e1828f9ce82c3fd4b507193d76b9f833cc
JOnathan Wade <jonathan@wade.id.au>
2021-12-01 21:26:58 +0800

    failed!!

--- 2021day1.py
+++ 2021day1.py
@@ -19,7 +19,13 @@ def sliding_window(elements, window_size):
 sliding_window(input, 3)
 
 
-
+# puzzle 2
+m = 0
+windows = [a * b * c for a, b, c in zip(input, input[1:], input[2:])]
+for (a, b) in zip(windows, windows[1:]):
+    if b > a:
+        m += 1
+print(m)
 
 
  
