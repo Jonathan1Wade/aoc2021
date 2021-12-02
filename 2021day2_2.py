ff474ab93ced1993fe51d0c43d415e9ea774409c
JOnathan Wade <jonathan@wade.id.au>
2021-12-02 15:47:10 +0800

    if and elif

--- 2021day2_2.py
+++ 2021day2_2.py
@@ -1,19 +1,27 @@
-#part1
+#part 2
 with open("2021day2.txt") as f:
     lines = f.readlines()
+    #print(lines)
 
 depth = 0
 forward = 0
+aim = 0
 
 for line in lines:
     key, value = line.split(" ")
+    #print(key, value)
 
     if key == "up":
-        depth -= int(value) 
+        #depth -= int(value)
+        aim -= int(value)
     elif key == "down":
-        depth += int(value)
+        #depth += int(value)
+        aim += int(value)
     elif key == "forward":
         forward += int(value)
+        depth += (int(value) * aim)
 
 print(depth * forward)
 
+    
+      
